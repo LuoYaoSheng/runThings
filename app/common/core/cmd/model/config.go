a95d0cf2110a51ecf1e5b298a792efca348556ee
寺西 <lys1988_cool@126.com>
2022-05-22 23:09:36 +0800

    Update config.go

--- app/common/core/cmd/model/config.go
+++ app/common/core/cmd/model/config.go
@@ -8,3 +8,18 @@ type RunThingsConf struct {
 	Cmd       string // 命令
 	Threshold string // 阈值
 }
+
+type RuleContent struct {
+	Property  string      `json:"property"`
+	Condition int         `json:"condition"`
+	Value     interface{} `json:"value"`
+}
+
+type Rule struct {
+	Id      int    `json:"id"`
+	Name    string `json:"name"`
+	Level   int    `json:"level"`
+	Code    string `json:"code"`
+	Sn      string `json:"sn"`
+	Content string `json:"content"` // 暂不适用 RuleContent
+}
